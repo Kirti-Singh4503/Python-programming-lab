e69b9d26e09f366efbd2e00a5ad9421a44dfe1c5
Kirti-Singh4503 <96518667+Kirti-Singh4503@users.noreply.github.com>
2022-06-01 14:51:13 +0530

    Update basic4.py

--- basic4.py
+++ basic4.py
@@ -1,16 +1,8 @@
-# Python code to print text/string using 
-# user-defined method
-
-# function defintion of "putMe()"
-# it will accepts a string and print it on the screen
-def putMe(text):
-    # printing text on the screen
-    print(text)
-
-# main code
-if __name__ == '__main__':
-    putMe('Hello world!')
-    putMe('Welcome @')
-    putMe('IncludeHelp')
-    putMe('The world of programming')
-    putMe('A programming community for developer and students')
+# printing integer value 
+print(12)
+# printing float value 
+print(12.56)
+# printing string value 
+print("Hello")
+# printing boolean value 
+print(True)
